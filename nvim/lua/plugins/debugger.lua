7a8fd8a4f9accdb16bb17fb61dcb0263caafd49d
Ian Dias <idias176@gmail.com>
2024-08-13 12:44:56 +0100

    Add dagster debugger

--- nvim/lua/plugins/debugger.lua
+++ nvim/lua/plugins/debugger.lua
@@ -6,4 +6,18 @@ dap.adapters.python = {
   args = { "-m", "debugpy.adapter" },
 }
 
+dap.configurations.python = dap.configurations.python or {}
+table.insert(dap.configurations.python, {
+  type = "python",
+  request = "launch",
+  name = "Dagster Dev",
+  module = "dagster",
+  args = { "dev", "-m", "orchestration" },
+  cwd = "${workspaceFolder}/dagster",
+  env = {
+    PYTHONPATH = "${workspaceFolder}",
+  },
+  console = "integratedTerminal",
+})
+
 return {}
