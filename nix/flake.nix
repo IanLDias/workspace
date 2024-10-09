c6c5c492949fdefb4b5af09e90cba5645092cb01
Ian Dias <idias176@gmail.com>
2024-10-09 15:36:08 +0100

    Add mac aliases for applications

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,9 +9,11 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
+
+
       environment.systemPackages =
         [
           pkgs.vim
@@ -19,8 +21,30 @@
           pkgs.tmux
           pkgs.tmuxinator
           pkgs.alacritty
+          pkgs.mkalias
         ];
 
+      # MacOS doesn't index symlinks for spotlight.
+      # This means that we can't search for GUI applications installed via nix unless we configure it like below
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+        # Set up applications.
+        echo "setting up /Applications..." >&2
+        rm -rf /Applications/Nix\ Apps
+        mkdir -p /Applications/Nix\ Apps
+        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+        while read src; do
+          app_name=$(basename "$src")
+          echo "copying $src" >&2
+          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+        done
+            '';
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
